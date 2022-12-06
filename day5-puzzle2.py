f2f0f9705016114310c1bd9d08174d30413bd04d
Ng Xiang Jun <ngxiangjun1999@gmail.com>
2022-12-06 13:16:45 +0800

    Add day 5 and 6

---
+++ day5-puzzle2.py
@@ -0,0 +1,50 @@
+from common_functions import solve
+from collections import deque
+
+DAY = 5
+
+
+def solver(data: str) -> str:
+    is_procedure = False
+    rows = []
+    procedures = []
+
+    for line in data.splitlines():
+        if line == "":
+            is_procedure = True
+            continue
+        if not is_procedure:
+            res = list(map(lambda x: x.strip(), [
+                       line[i: i + 4] for i in range(0, len(line), 4)]))
+            rows.append(res)
+
+        else:
+            procedures.append(line.split())
+
+    stacks = [[] for _ in range(len(rows[0]))]
+
+    # create stacks
+    for row in reversed(rows[:-1]):
+        for i in range(len(row)):
+            crate = row[i]
+            if crate != "":
+                stacks[i].append(crate[1])
+
+    # change procedures to neater form.
+    # index 0 - how many to move
+    # index 1 - where to move from
+    # index 2 - where to move to
+    procedures = list(
+        map(lambda p: [int(p[1]), int(p[3]) - 1, int(p[5]) - 1], procedures))
+    for procedure in procedures:
+        tomove = deque()
+        for _ in range(procedure[0]):
+            tomove.appendleft(stacks[procedure[1]].pop())
+        stacks[procedure[2]].extend(tomove)
+
+    return "".join(list(map(lambda s: s.pop() if len(s) > 0 else "", stacks)))
+
+
+if __name__ == "__main__":
+    output = solve(DAY, solver)
+    print(output)
---
+++ day6-puzzle2.py
@@ -0,0 +1,15 @@
+from common_functions import solve
+
+DAY = 6
+
+
+def solver(data: str) -> str:
+    SIZE = 14
+    for i in range(len(data) - SIZE):
+        if len(set(data[i: i + SIZE])) == SIZE:
+            return i + SIZE
+
+
+if __name__ == "__main__":
+    output = solve(DAY, solver)
+    print(output)
