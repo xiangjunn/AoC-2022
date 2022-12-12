ae0295e2da6223eab7956287325046e942729fae
Ng Xiang Jun <ngxiangjun1999@gmail.com>
2022-12-12 14:23:31 +0800

    Add day 11 and 12

---
+++ day11-puzzle1.py
@@ -0,0 +1,80 @@
+from common_functions import solve
+from collections import deque
+
+DAY = 11
+
+
+def solver(data: str):
+    curr_monkey = None
+    for line in data.splitlines():
+        if line.startswith("Monkey"):
+            curr_monkey = Monkey()
+        elif "Starting items" in line:
+            items = line.split(":")[1]
+            items = deque(map(int, items.split(",")))
+            curr_monkey.items = items
+        elif "Operation" in line:
+            operation = line.split("=")[1]
+            curr_monkey.operation = operation
+        elif "Test" in line:
+            mod = int(line.split("divisible by")[1])
+            curr_monkey.mod = mod
+        elif "If true" in line:
+            next_monkey = int(line.split("throw to monkey")[1])
+            curr_monkey.if_true = next_monkey
+        elif "If false" in line:
+            next_monkey = int(line.split("throw to monkey")[1])
+            curr_monkey.if_false = next_monkey
+
+    # Starts simulation
+    rounds = 20
+    Monkey.run_simulation(rounds)
+
+    inspection_count = list(map(lambda m: m.inspected_count, Monkey.monkeys))
+    inspection_count.sort()
+    return inspection_count[-1] * inspection_count[-2]
+
+
+class Monkey:
+    monkeys = []
+
+    def __init__(self):
+        Monkey.monkeys.append(self)
+        self.items = []
+        self.operation = None
+        self.mod = None
+        self.if_true = None
+        self.if_false = None
+        self.inspected_count = 0
+
+    def start_inspection(self):
+        while len(self.items) != 0:
+            worry = self.items.popleft()
+            self.inspected_count += 1
+            worry = self.apply_operation(worry)
+            worry //= 3
+            # throw to next monkey
+            if worry % self.mod == 0:
+                next_monkey = Monkey.monkeys[self.if_true]
+                next_monkey.add_item(worry)
+            else:
+                next_monkey = Monkey.monkeys[self.if_false]
+                next_monkey.add_item(worry)
+
+    def add_item(self, worry):
+        self.items.append(worry)
+
+    def apply_operation(self, old):
+        # Argument must be named as old for eval to work
+        return eval(self.operation)
+
+    @classmethod
+    def run_simulation(cls, rounds):
+        for _ in range(rounds):
+            for monkey in Monkey.monkeys:
+                monkey.start_inspection()
+
+
+if __name__ == "__main__":
+    output = solve(DAY, solver)
+    print(output)
---
+++ day12-puzzle2.py
@@ -0,0 +1,54 @@
+from common_functions import solve
+from collections import deque
+import sys
+
+DAY = 12
+
+
+def solver(data: str):
+    grid = []
+    start = []
+    for i, line in enumerate(data.splitlines()):
+        for j, s in enumerate(line):
+            if s == "a":
+                start.append((i, j))
+        grid.append(line)
+
+    smallest_step = sys.maxsize
+    for coordinate in start:
+        smallest_step = min(smallest_step, bfs(grid, coordinate))
+    return smallest_step
+
+
+def bfs(grid, start):
+    queue = deque()
+    x, y = start
+    queue.append((x, y, 0))
+    visited = [[False for _ in row] for row in grid]
+    visited[x][y] = True
+    directions = [1, 0, -1, 0, 1]
+    while len(queue) > 0:
+        i, j, step = queue.popleft()
+        curr = grid[i][j]
+        for d in range(4):
+            new_i = i + directions[d]
+            new_j = j + directions[d + 1]
+            if new_i < 0 or new_i >= len(grid) or new_j < 0 or new_j >= len(grid[0]):
+                continue
+            square = grid[new_i][new_j]
+            if square == "E" and curr == "z":
+                # found end goal
+                return step + 1
+            elif grid[i][j] == "S" and square == "a":
+                visited[new_i][new_j] = True
+                queue.append((new_i, new_j, step + 1))
+            elif not visited[new_i][new_j] and grid[i][j] != "S" and ord(square) - ord(curr) <= 1:
+                visited[new_i][new_j] = True
+
+                queue.append((new_i, new_j, step + 1))
+    return sys.maxsize
+
+
+if __name__ == "__main__":
+    output = solve(DAY, solver)
+    print(output)
