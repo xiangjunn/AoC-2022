1e0a1848e98b33932c94ef997373e6b698b74c51
Ng Xiang Jun <ngxiangjun1999@gmail.com>
2022-12-19 18:26:40 +0800

    Update day 8

--- day8-puzzle2.py
+++ day8-puzzle2.py
@@ -1,4 +1,5 @@
 from common_functions import solve
+import sys
 
 DAY = 8
 
@@ -19,36 +20,61 @@ def solver(data: str) -> str:
 
 def calculate_scores_1d(index, arr, scores, is_row):
     stack = []
+    arr.append(sys.maxsize)
+    same_val_leftbound = {}
     for i, height in enumerate(arr):
         while len(stack) > 0 and height >= arr[stack[-1]]:
             curr = stack.pop()
-            left = search_prev_higher(curr, arr)
-            right = i
+            if arr[curr] == height:
+                same_val_leftbound[i] = curr
+            if curr in same_val_leftbound:
+                left = same_val_leftbound[curr]
+            else:
+                left = stack[-1] if len(stack) > 0 else 0
+            right = min(i, len(arr) - 2)
             score_multiplier = (curr - left) * (right - curr)
             if is_row:
                 scores[index][curr] *= score_multiplier
             else:
                 scores[curr][index] *= score_multiplier
         stack.append(i)
-    right = len(arr) - 1
-    while len(stack) > 0:
-        curr = stack.pop()
-        left = search_prev_higher(curr, arr)
-        score_multiplier = (curr - left) * (right - curr)
-        if is_row:
-            scores[index][curr] *= score_multiplier
-        else:
-            scores[curr][index] *= score_multiplier
-
-
-def search_prev_higher(index, arr):
-    for i in range(index - 1, -1, -1):
-        if arr[i] >= arr[index]:
-            return i
-    return 0
 
 
 # Method 2
+"""
+def solver(data: str) -> str:
+    grid = list(map(lambda row: list(map(int, list(row))), data.splitlines()))
+    grid_transpose = list(
+        map(lambda i: list(map(lambda x: x[i], grid)), range(len(grid[0]))))
+    scores = [[1 for _ in row] for row in grid]
+    for i in range(len(grid)):
+        calculate_scores_1d(i, grid[i], scores, True, False)
+        calculate_scores_1d(i, grid[i][::-1], scores, True, True)
+    for j in range(len(grid_transpose)):
+        calculate_scores_1d(j, grid_transpose[j], scores, False, False)
+        calculate_scores_1d(j, grid_transpose[j][::-1], scores, False, True)
+    return max(max(scores, key=max))
+
+
+def calculate_scores_1d(index, array, scores, is_row, is_reversed):
+    stack = []
+    arr = array.copy()
+    arr.append(sys.maxsize)
+    for i, height in enumerate(arr):
+        while len(stack) > 0 and height >= arr[stack[-1]]:
+            curr = stack.pop()
+            right = min(i, len(arr) - 2)
+            score_multiplier = right - curr
+            # update curr to proper index
+            curr = len(array) - 1 - curr if is_reversed else curr
+            if is_row:
+                scores[index][curr] *= score_multiplier
+            else:
+                scores[curr][index] *= score_multiplier
+        stack.append(i)
+"""
+
+# Method 3
 """
 def solver(data: str) -> str:
     grid = list(map(lambda row: list(map(int, list(row))), data.splitlines()))
